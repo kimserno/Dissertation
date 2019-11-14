9854037bc8096f87f80a2321b6dccdbe1aa9fd3c
Kim Serno <kimserno@gmail.com>
2019-11-14 12:00:32 -0600

    added files

---
+++ BRSP_data.R
@@ -0,0 +1,106 @@
+
+
+library(tidyverse)
+
+setwd("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/BBS_manipulation")
+
+
+#subset routes to those in Great Plains US
+BBSroutes<-read.csv("data_files/Unzipped/routes.csv")
+head(BBSroutes)
+
+BBSroutes$RouteID<-((BBSroutes$StateNum*1000)+BBSroutes$Route)
+head(BBSroutes)
+write.csv(BBSroutes, file = "BBS_routes.csv")
+
+BRSP_BBSroutes<-read.csv("data_files/Unzipped/BRSP_BBSroutes2.csv")
+head(BRSP_BBSroutes)
+
+BRSProutes<-left_join(x=BRSP_BBSroutes, y=BRSP_BBSroutes, by ="RouteID")
+head(BRSProutes)
+
+
+#State BBS data:
+#import states:
+Arizona<-read.csv("data_files/Unzipped/BBS_states/Arizona.csv")
+California<-read.csv("data_files/Unzipped/BBS_states/Califor.csv")
+Colorado<-read.csv("data_files/Unzipped/BBS_states/Colorad.csv")
+Idaho<-read.csv("data_files/Unzipped/BBS_states/Idaho.csv")
+Kansas<-read.csv("data_files/Unzipped/BBS_states/Kansas.csv")
+Montana<-read.csv("data_files/Unzipped/BBS_states/Montana.csv")
+NDakota<-read.csv("data_files/Unzipped/BBS_states/NDakota.csv")
+Nebraska<-read.csv("data_files/Unzipped/BBS_states/Nebrask.csv")
+Nevada<-read.csv("data_files/Unzipped/BBS_states/Nevada.csv")
+NMexico<-read.csv("data_files/Unzipped/BBS_states/NMexico.csv")
+Oregon<-read.csv("data_files/Unzipped/BBS_states/Oregon.csv")
+SDakota<-read.csv("data_files/Unzipped/BBS_states/SDakota.csv")
+Utah<-read.csv("data_files/Unzipped/BBS_states/Utah.csv")
+Washington<-read.csv("data_files/Unzipped/BBS_states/Washing.csv")
+Wyoming<-read.csv("data_files/Unzipped/BBS_states/Wyoming.csv")
+
+#subset to states of interest
+SGstates<-rbind(Arizona, California, Colorado, Idaho, Kansas, Montana, NDakota, Nebraska, Nevada, NMexico, Oregon, SDakota, Utah, Washington, Wyoming)
+SGstates$RouteID<-((SGstates$StateNum*1000)+SGstates$Route)
+head(SGstates)
+length(unique(SGstates$RouteID))
+
+SBstates<-subset(SGstates, RPID == 101)
+length(unique(SBstates$RouteID))
+
+##subset to years of interest 2009-2013:
+routes2009<-subset(SBstates, Year == 2009)
+routes2010<-subset(SBstates, Year == 2010)
+routes2011<-subset(SBstates, Year == 2011)
+routes2012<-subset(SBstates, Year == 2012)
+routes2013<-subset(SBstates, Year == 2013)
+#routes surveyed per year:
+length(unique(routes2009$RouteID))
+length(unique(routes2010$RouteID))
+length(unique(routes2011$RouteID))
+length(unique(routes2012$RouteID))
+length(unique(routes2013$RouteID))
+
+
+
+
+df<-data.frame(matrix(ncol = 1, nrow = 1555))
+
+df$allroutes<-unique(SBstates$RouteID)
+df$r2009<-0
+df$r2010<-0
+df$r2011<-0
+df$r2012<-0
+df$r2013<-0
+df<-df[,-1]
+
+c2009<-df$allroutes %in% routes2009$RouteID
+df$r2009[c2009]<-1
+c2010<-df$allroutes %in% routes2010$RouteID
+df$r2010[c2010]<-1
+c2011<-df$allroutes %in% routes2011$RouteID
+df$r2011[c2011]<-1
+c2012<-df$allroutes %in% routes2012$RouteID
+df$r2012[c2012]<-1
+c2013<-df$allroutes %in% routes2013$RouteID
+df$r2013[c2013]<-1
+df$sum<-rowSums(df[,2:6])
+head(df)
+table(df$sum)
+sum(df$r2009)
+sum(df$r2010)
+sum(df$r2011)
+sum(df$r2012)
+sum(df$r2013)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ GIS_BRSP.R
@@ -0,0 +1,128 @@
+
+
+library(rgdal)
+library(raster)
+library(sp)
+library(prism)
+library(rgeos)
+#setwd("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation")
+setwd("~/Dropbox/Baylor/PhD/Dissertation")
+
+#load in gap lc rasters to mosaic together for gaplc west raster
+# R2<-raster("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/GIS/unzipped/gap_landcover/Region2_California/gaplf2011lc_v30_lcc_2.tif")
+# R11<-raster("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/GIS/unzipped/gap_landcover/Region11_NorthPacific/gaplf2011lc_v30_lcc_11.tif")
+# R13<-raster("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/GIS/unzipped/gap_landcover/Region13_PlainsPrairiePotholes/gaplf2011lc_v30_lcc_13.tif")
+# R15<-raster("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/GIS/unzipped/gap_landcover/Region15_SouthernRockies/gaplf2011lc_v30_lcc_15.tif")
+# R3<-raster("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/GIS/unzipped/gap_landcover/Region3_Desert/gaplf2011lc_v30_lcc_3.tif")
+# R5<-raster("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/GIS/unzipped/gap_landcover/Region5_GreatBasin/gaplf2011lc_v30_lcc_5.tif")
+# R6<-raster("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/GIS/unzipped/gap_landcover/Region6_GreatNorthern/gaplf2011lc_v30_lcc_6.tif")
+# R7<-raster("C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/GIS/unzipped/gap_landcover/Region7_GreatPlains/gaplf2011lc_v30_lcc_7.tif")
+
+# R2<-raster("~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Region2_California/gaplf2011lc_v30_lcc_2.tif")
+# R11<-raster("~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Region11_NorthPacific/gaplf2011lc_v30_lcc_11.tif")
+# R13<-raster("~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Region13_PlainsPrairiePotholes/gaplf2011lc_v30_lcc_13.tif")
+# R15<-raster("~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Region15_SouthernRockies/gaplf2011lc_v30_lcc_15.tif")
+# R3<-raster("~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Region3_Desert/gaplf2011lc_v30_lcc_3.tif")
+# R5<-raster("~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Region5_GreatBasin/gaplf2011lc_v30_lcc_5.tif")
+# R6<-raster("~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Region6_GreatNorthern/gaplf2011lc_v30_lcc_6.tif")
+# R7<-raster("~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Region7_GreatPlains/gaplf2011lc_v30_lcc_7.tif")
+# plot(R2)
+# proj4string(R2)
+# proj4string(R11)
+# proj4string(R13)
+# proj4string(R15)
+# proj4string(R3)
+# proj4string(R5)
+# proj4string(R6)
+# proj4string(R7)
+
+#Creation of gaplc_west raster and timing
+# start.time = Sys.time()
+# Gaplc_West_raster<-mosaic(R2, R11, R13, R15, R3, R5, R6, R7, fun = mean,  filename = "~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Gaplc_West_raster.tif")
+# end.time = Sys.time(); elapsed.time = round(difftime(end.time,start.time, units='mins'), dig = 2)
+# paste('Elapsed time was ', elapsed.time, 'minutes or ', round(elapsed.time/60,3), 'hours', sep = '')
+# r2r11<-mosaic(R2, R11, fun = mean)
+#brsp_GDB<-"C:/Users/kim_serno1/Documents/GIS_projects/Dissertation/data/BRSP.gdb"
+# brsp_GDB<-"C:/Users/kim_serno1/Dropbox/Baylor/PhD/Dissertation/GIS/BRSP.gdb"
+brsp_GDB<-"~/Dropbox/Baylor/PhD/GIS/BRSP.gdb"
+
+
+subset(ogrDrivers(), grepl("GDB", name))
+fc_list<-ogrListLayers(brsp_GDB)
+print(fc_list)
+
+us<-readOGR(dsn=brsp_GDB, layer = "US_48States")
+plot(us)
+summary(us)
+
+BRSP_breeding<-readOGR(dsn=brsp_GDB, layer = "BRSP_BreedingUS")
+plot(BRSP_breeding)
+summary(BRSP_breeding)
+
+BRSP_routes<-readOGR(dsn=brsp_GDB, layer = "BBS_routes_BRSP")
+plot(BRSP_routes, add = TRUE)
+summary(BRSP_routes)
+
+BRSP_mincenter<-readOGR(dsn=brsp_GDB, layer = "BRSP_MinimumBound_center")
+plot(BRSP_mincenter, add = TRUE)
+summary(BRSP_mincenter)
+
+# Load in Gap Landcover west raster (creation code above)
+# Gaplc_west<-raster("C:/Users/kim_serno1/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Gaplc_West_raster.tif")
+Gaplc_west<-raster("~/Dropbox/Baylor/PhD/GIS/unzipped/gap_landcover/Gaplc_West_raster.tif")
+
+# Get projection information for gap landcover raster:
+CRS_gap<-proj4string(Gaplc_west)
+CRS_gap
+
+US<-spTransform(us, CRS = CRS_gap)
+BRSP_Breeding<-spTransform(BRSP_breeding, CRS = CRS_gap)
+BRSP_Routes<-spTransform(BRSP_routes, CRS = CRS_gap)
+BRSP_Mincenter<-spTransform(BRSP_mincenter, CRS = CRS_gap)
+proj4string(US)
+proj4string(BRSP_Breeding)
+proj4string(BRSP_Routes)
+proj4string(BRSP_Mincenter)
+
+# plot land cover raster with BRSP breeding range and BBS routes overlay
+plot(Gaplc_west)
+plot(BRSP_Breeding, add = TRUE)
+plot(BRSP_Routes, add = TRUE)
+
+# Buffers around minimum bounding rectangle center (20km, 40km)
+BRSP_20km<-gBuffer(BRSP_Mincenter, width = 20000, byid = TRUE, id=BRSP_Mincenter@data$RouteID)
+plot(BRSP_20km, add = TRUE)
+summary(BRSP_20km)
+BRSP_40km<-gBuffer(BRSP_Mincenter, width = 40000, byid = TRUE, id=BRSP_Mincenter@data$RouteID)
+plot(BRSP_40km, add = TRUE)
+summary(BRSP_40km)
+
+#buffers around routes
+BRSP_1km<-gBuffer(BRSP_Routes, width = 1000, byid = TRUE, id=BRSP_Routes@data$RouteID)
+plot(BRSP_1km, add = TRUE)
+summary(BRSP_1km)
+
+BRSP_400m<-gBuffer(BRSP_Routes, width = 400, byid = TRUE, id=BRSP_Routes@data$RouteID)
+plot(BRSP_400m, add = TRUE)
+summary(BRSP_400m)
+
+BRSP_5km<-gBuffer(BRSP_Routes, width = 5000, byid = TRUE, id=BRSP_Routes@data$RouteID)
+plot(BRSP_5km, add = TRUE)
+summary(BRSP_5km)
+
+
+#extract gap lc values for buffer:
+start.time = Sys.time()
+e400m<-extract(Gaplc_west, BRSP_400m)
+end.time = Sys.time(); elapsed.time = round(difftime(end.time,start.time, units='mins'), dig = 2)
+paste('Elapsed time was ', elapsed.time, 'minutes or ', round(elapsed.time/60,3), 'hours', sep = '')
+
+start.time = Sys.time()
+e400mTable<-lapply(e400m, table)
+end.time = Sys.time(); elapsed.time = round(difftime(end.time,start.time, units='mins'), dig = 2)
+paste('Elapsed time was ', elapsed.time, 'minutes or ', round(elapsed.time/60,3), 'hours', sep = '')
+
+head(e400mTable)
+
+test<-extract(Gaplc_west, BRSP_breeding, method = 'simple')
+test<-as.matrix(table(values(Gaplc_west)))
